899a8ccf05772feff9650a6dccc3ffbbb30da6a1
UroojMurtaz <uroojmurtaza12@gmail.com>
2023-10-04 21:57:06 +0500

    Experience-Design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utlis/motion";
+import { SectionWrapper } from "../hoc";
 
-const About = () => {
-  return (
-    <div>About</div>
+const ServiceCard=({index,title,icon})=>{
+  return(
+   <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+    variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+    className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+    <div
+    options={{
+      max: 25,
+      scale: 1,
+      speed: 450     
+    }}
+    className="bg-tertiary py-5 px-12 rounded-[20px] min-h-[280px] 
+    flex justify-evenly items-center flex-col 
+    "
+    >
+      <img
+      src={icon}
+      alt={title}
+      className="w-16 h-16 object-contain"
+      />
+      <h3 
+      className="text-center text=-[20px] text-white">
+        {title}
+        </h3>
+
+    </div>
+    </motion.div>
+
+   </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introdution</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p 
+      className="mt-4 text-secondary text-[17px] leading-[30px] max-w-3xl"
+      variants={fadeIn("", "", 0.1, 1)}>
+        I am software Developer with experience in building
+        websites and web applications. I specialize in JavaScript and have
+        professional experience working with React, Redux, Next.js, and Node.js.
+        I ampassionate about building scalable and maintainable software, creating
+        effective solutions, and learning every day to grow professionally in
+        the IT industry.
+      </motion.p>
+      <div
+      className="mt-20 flex flex-wrap gap-10"
+      >
+        {
+          services.map((service,index)=>(
+            <ServiceCard key={service.title} index={index}
+            {...service}
+            />
+          ))
+        }
+
+
+
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About,"about");
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 `}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
@@ -21,8 +21,18 @@ const Hero = () => {
           </p>
         </div>
       </div>
-        <ComputerCanvas />
-        
+      <ComputerCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] border-4 border-secondary rounded-3xl flex justify-center items-start p-2">
+            <motion.div
+            animate={{y:[0,24,0]}}
+            transition={{repeat:Infinity, duration:1.5,repeatType:"loop"}}
+              className="w-3 h-3 bg-secondary rounded-full"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
